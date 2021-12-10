2f67ea7dc563681d12869cfd5ef7a9dc270561d8
GerbH57 <92798943+GerbH57@users.noreply.github.com>
2021-12-10 08:47:52 -0500

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,12 @@
+// Sends string and plays alarm
 radio.onReceivedString(function (receivedString) {
     Recived_word = receivedString
-    for (let index = 0; index < 400000000000000000000; index++) {
+    for (let index = 0; index < 1e+308; index++) {
         music.playMelody("E B C5 A B G A F ", 1e+308)
         basic.showString(receivedString)
     }
 })
+// sets radio group to 1
 let Recived_word = ""
 radio.setGroup(1)
 pins.setAudioPin(AnalogPin.P0)
-basic.forever(function () {
-	
-})
