e7c998c7b526847544bafe36f0ad910bfce51a40
GerbH57 <92798943+GerbH57@users.noreply.github.com>
2021-12-10 08:43:45 -0500

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,13 @@
 radio.onReceivedString(function (receivedString) {
     Recived_word = receivedString
-    basic.showString("Burger")
+    for (let index = 0; index < 400000000000000000000; index++) {
+        music.playMelody("E B C5 A B G A F ", 1e+308)
+        basic.showString(receivedString)
+    }
 })
 let Recived_word = ""
 radio.setGroup(1)
+pins.setAudioPin(AnalogPin.P0)
+basic.forever(function () {
+	
+})
